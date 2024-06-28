6c1d3a14303867a6073bd6672ee896df544c2dfa
ismael strey pereira <ismaelstrey@hotmail.com>
2024-06-28 16:42:54 -0300

    crate function

---
+++ src/funcoes.ts
@@ -0,0 +1,39 @@
+// const criarUsuario = (usuario: Usuario): void => {
+//     // Lógica para criar um usuário
+// };
+
+// const atualizarUsuario = (id: number, usuario: Partial<Usuario>): void => {
+//     // Lógica para atualizar um usuário
+// };
+
+// const criarEmpresa = (empresa: Empresa): void => {
+//     // Lógica para criar uma empresa
+// };
+
+// const atualizarEmpresa = (id: number, empresa: Partial<Empresa>): void => {
+//     // Lógica para atualizar uma empresa
+// };
+
+// const criarOLT = (olt: OLT): void => {
+//     // Lógica para criar um OLT
+// };
+
+// const atualizarOLT = (id: number, olt: Partial<OLT>): void => {
+//     // Lógica para atualizar um OLT
+// };
+
+// const criarCidade = (cidade: Cidade): void => {
+//     // Lógica para criar uma cidade
+// };
+
+// const atualizarCidade = (id: number, cidade: Partial<Cidade>): void => {
+//     // Lógica para atualizar uma cidade
+// };
+
+// const criarBairro = (bairro: Bairro): void => {
+//     // Lógica para criar um bairro
+// };
+
+// const atualizarBairro = (id: number, bairro: Partial<Bairro>): void => {
+//     // Lógica para atualizar um bairro
+// };
