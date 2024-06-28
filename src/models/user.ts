d0a856262b865c982ee8b22457b7df74e24fced1
ismael strey pereira <ismaelstrey@hotmail.com>
2024-06-28 13:09:02 -0300

    update api

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,5 +1,55 @@
+interface Empresa {
+    id: number;
+    nome: string;
+    endereco: string;
+    telefone: string;
+    email: string;
+    olts: OLT[];
+    bairroId: number;
+    bairro: Bairro;
+    usuarios: User[];
+}
+
+interface OLT {
+    id: number;
+    name: string;
+    oltIp: string;
+    reachableViaVpn: boolean;
+    telnetTcpPort: string;
+    oltTelnetUsername: string;
+    oltTelnetPassword: string;
+    snmpReadOnlyCommunity: string;
+    snmpReadWriteCommunity: string;
+    snmpUdpPort: string;
+    iptvModule: boolean;
+    oltHardwareVersion: string;
+    oltSoftwareVersion: string;
+    supportedPonTypes: string;
+    empresaId?: number;
+    empresa?: Empresa;
+}
+
 export interface User {
     id: number;
     nome: string;
     email: string;
+    senha?: string;
+    dataDeNascimento?: Date;
+    telefone?: string;
+    empresaId?: number;
+    empresa?: Empresa;
+}
+
+interface Cidade {
+    id: number;
+    nome: string;
+    bairros: Bairro[];
+}
+
+interface Bairro {
+    id: number;
+    nome: string;
+    cidadeId: number;
+    cidade: Cidade;
+    empresas: Empresa[];
 }
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,17 +1,21 @@
 // src/services/userService.ts
-
 import { prisma } from '../prisma';
 import { User } from '../models/user';
-
-export const getAllUsers = async (): Promise<User[]> => {
-
-
-    const data = await prisma.usuario.findMany({
+export const getAllUsers = async (): Promise<User[]> => await prisma.usuario.findMany(
+    {
         select: { id: true, nome: true, email: true }
-    })
-
-    return data
-}
+    }
+)
+// export const createUser = async (user: User): Promise<User> => {
+//     try {
+//         const createdUser = await prisma.usuario.create({
+//             data: user,
+//         })
+//         return createdUser;
+//     } catch (error: any) {
+//         throw new Error(`Error creating user: ${error.message}`);
+//     }
+// };
 
 export const getUserById = async (id: number): Promise<User | null> => await prisma.usuario.findFirst(
     {
